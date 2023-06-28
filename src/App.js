7353b801d21bee365b2f7ce71c6cffd5a2555b01
kaisheng1989 <kaisheng1989@gmail.com>
2023-06-28 17:27:41 +0800

    adjust login

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ function RequireAuth({ children, redirectTo, user }) {
         <Navbar handleSignOut={handleSignOut} user={user} />
         <Routes>
           {/* Setting the local host 300 main page. */}
-          <Route path="/" element={<Welcome />} />
+          <Route path="/instagram-bootcamp/" element={<Welcome />} />
 
           {/*Login in component */}
           <Route
